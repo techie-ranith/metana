7589a33432ad8936d098b146f040c572ee99a6d6
techie-ranith <ranith2k3@gmail.com>
2025-03-05 11:37:16 +0530

    adding logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 // import Image from "next/image";
 import Form from "@/components/forms/cv_upload_form";
+import Image from "next/image";
+import Metana from "@/public/logo.png";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
+    <div className="flex flex-col items-center justify-center w-full h-screen py-10">
+      <Image src={Metana} alt="Metana Logo" />
       <Form></Form>
+      <div className="w-full h-screen relative">
+    </div>
     </div>
   );
 }
--- components/forms/cv_upload_form.tsx
+++ components/forms/cv_upload_form.tsx
@@ -12,9 +12,9 @@ import {
   zodResolver
 } from "@hookform/resolvers/zod"
 import * as z from "zod"
-import {
-  cn
-} from "@/lib/utils"
+// import {
+//   cn
+// } from "@/lib/utils"
 import {
   Button
 } from "@/components/ui/button"
